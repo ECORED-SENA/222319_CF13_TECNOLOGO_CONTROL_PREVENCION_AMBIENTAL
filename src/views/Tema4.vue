b2944a7b8b380a5a816e5fad4bd1b9be0aa1e16f
Va-Gomez <cmgomez72@misena.edu.co>
2022-12-26 10:11:02 -0500

    Actualizado

--- src/views/Tema4.vue
+++ src/views/Tema4.vue
@@ -3,23 +3,56 @@
   BannerInterno
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5
     
-    #t_3_1.pb-5.h2 tema 3.1
-    .pb-5(v-for="item of 15")
+    .titulo-principal.color-acento-contenido
+      .titulo-principal__numero
+        span 4
+      h1 Aprovechamiento y tratamiento residuos peligrosos
 
-    #t_3_2.pb-5.h2 tema 3.2
-    .pb-5(v-for="item of 15")
+    p.mb-4 El personal que labora en las empresas de aprovechamiento y tratamiento de residuos está propenso a varios riesgos, por ende, es importante el uso de elementos de protección personal y seguridad y salud en los ambientes de trabajo.
 
-    #t_3_3.pb-5.h2 tema 3.3
-    .pb-5(v-for="item of 15")
-
-    #t_3_4.pb-5.h2 tema 3.4
-    .pb-5(v-for="item of 15")
-
-    #t_3_5.pb-5.h2 tema 3.5
-    .pb-5(v-for="item of 15")
-
-    #t_3_6.pb-5.h2 tema 3.6
-    .pb-5(v-for="item of 15")
+    .row.justify-content-center.mb-4
+      .col-lg-10.mb-2
+        h5.mb-0 Tabla 15
+        span.mb-2 Riesgos en la labor
+      .col-lg-8
+        .tabla-a.color-acento-botones.mb-2
+          table
+            thead
+              tr(style='background-color: #80CAB6')
+                th Riesgos
+                th Observaciones
+            tbody(style='background-color: #ECF7F4')
+              tr
+                td.text-center <b>Físico ruido</b>>
+                td Ruido de equipos y maquinaria en general en la planta de reciclaje, como es el caso de romper el vidrio, triturar algún material.
+                  p Se requiere de protección acústica.
+              tr
+                td.text-center <b>Biológico</b>>
+                td Mordedura de algún animal, como los roedores, porque, en algunos casos, el almacenamiento de materiales, como envases, llega con residuos que atraen estos vectores.
+                  p Se requiere manejo de plagas en el lugar y estar vacunado contra la hepatitis b.
+              tr
+                td.text-center <b>De seguridad mecánico</b>>
+                td Aplastamiento o corte de alguna extremidad, como las manos, con herramientas o maquinaria.
+                  p Protección, demarcación de áreas, uso de casco, nivel de apilado requerido según norma, amarras de la mercancía, sensores de movimiento en la maquinaria, capacitación al personal de la labor, guantes de carnaza, overol y no uso de joyas o accesorios que puedan crear atrapamientos en la maquinaria.
+              tr
+                td.text-center <b>Físico  iluminación</b>>
+                td Por exceso al aire libre o por defecto. Si es por exceso, uso de monogafas con filtro solar; y si es por defecto, mejor iluminación del lugar.
+              tr
+                td.text-center <b>Físico vibración</b>
+                td La vibración de la maquinaria. El operario tiene que entrar en contacto físico con la máquina, razón por la cual sufre mayor exposición a vibración en cuerpo total. Uso de elementos que mitiguen la vibración. 
+              tr
+                td.text-center <b>Biomecánico movimiento repetitivo</b>
+                td Movimientos repetitivos en las actividades requieren pausas activas.
+              tr
+                td.text-center <b>Biomecánico sobreesfuerzo</b>
+                td Manejo manual de cargas pesadas, uso de postura adecuada y control de peso
+              tr
+                td.text-center <b>Seguridad locativa</b>
+                td Caída de objetos , caída a nivel de piso, uso de casco y botas
+              tr
+                td.text-center <b>Químicos</b>
+                td Inhalación o contacto con la piel de sustancias químicas que se encuentren en los materiales. Se requiere de mascarilla de gases y guantes resistentes a químicos. 
+        figcaption Nota. ANDI (2017) Calidades de materiales para reciclaje. p.7
 
 </template>
 
--- src/views/Tema5.vue
+++ src/views/Tema5.vue
@@ -3,23 +3,143 @@
   BannerInterno
   .container.tarjeta.tarjeta--blanca.p-4.p-md-5
     
-    #t_3_1.pb-5.h2 tema 3.1
-    .pb-5(v-for="item of 15")
+    .titulo-principal.color-acento-contenido
+      .titulo-principal__numero
+        span 5
+      h1 Economía circular
 
-    #t_3_2.pb-5.h2 tema 3.2
-    .pb-5(v-for="item of 15")
+    p.mb-4 La economía circular es una estrategia innovadora de diseño, producción y creación de valor, que beneficia a las empresas, la sociedad y el medio ambiente, reemplazando el esquema clásico de producción de la “economía lineal”. En la “economía lineal”, se extraen recursos, se producen bienes, se consumen y al final se desechan. 
 
-    #t_3_3.pb-5.h2 tema 3.3
-    .pb-5(v-for="item of 15")
+    .bloque-texto-g.color-acento-contenido.p-4.p-sm-4.p-md-5.mb-4 
+      .bloque-texto-g__img(
+        :style="{'background-image': `url(${require('@/assets/curso/tema5/img1.jpg')})`}"
+      )
+      .bloque-texto-g__texto.p-5
+        p.mb-0 La economía circular, en cambio, busca optimizar el uso de insumos, incrementar la vida útil de los productos y, al final, por medio de la recirculación de materiales, no desperdiciar nada y reutilizarlo todo. Con esto, las empresas mitigan sus impactos ambientales, optimizan sus costos, generan nuevos ingresos e incrementan su rentabilidad.
 
-    #t_3_4.pb-5.h2 tema 3.4
-    .pb-5(v-for="item of 15")
+    .row.justify-content-center.mb-4 
+      .col-lg-10
+        h5.mb-0 Figura 11
+        span Economía lineal vs economía circular
+        figure.mb-2
+          img(src='@/assets/curso/tema5/fig1.jpg', alt='Texto que describa la imagen')
 
-    #t_3_5.pb-5.h2 tema 3.5
-    .pb-5(v-for="item of 15")
+    p.mb-4 A continuación, profundice en algunos aspectos del orden operativo que refuerzan los principios de la economía circular.
 
-    #t_3_6.pb-5.h2 tema 3.6
-    .pb-5(v-for="item of 15")
+    .row.justify-content-center.mb-4 
+      .col-lg-10
+        .tarjeta.color-8.p-4
+          h4.text-center Las 9 R de la economía circular
+          p.text-center Cuando se habla de economía circular, es muy probable que también se hable de las nueve erres de la misma. Las 9 R de la economía circular son niveles básicos a tener presentes.
+          figure.mb-2
+            img(src='@/assets/curso/tema5/fig.svg', alt='Texto que describa la imagen')
+
+    p.mb-4 <b>Algunos ejemplos concretos de economía circular son:</b>
+
+    .row.align-items-center.justify-content-center.mb-0
+      .col-lg-3.mb-4
+        .tarjeta.color-11.py-2.px-0 
+          .row.align-items-center
+            .col-lg-4
+              figure
+                img(src='@/assets/curso/tema5/1.svg', alt='Texto que describa la imagen')
+            .col-lg-8
+              .row
+                .col-lg-11
+                  p.mb-0 Neumáticos viejos transformados en suelas de zapatos.
+      .col-lg-3.mb-4
+        .tarjeta.color-11.py-2.px-0 
+          .row.align-items-center
+            .col-lg-4
+              figure
+                img(src='@/assets/curso/tema5/2.svg', alt='Texto que describa la imagen')
+            .col-lg-8
+              .row
+                .col-lg-11
+                  p.mb-0 Ropa usada como material para fabricar aislantes térmicos en bioconstrucción.
+      .col-lg-3.mb-4
+        .tarjeta.color-11.py-2.px-0 
+          .row.align-items-center
+            .col-lg-4
+              figure
+                img(src='@/assets/curso/tema5/3.svg', alt='Texto que describa la imagen')
+            .col-lg-8
+              .row
+                .col-lg-11
+                  p.mb-0 Caucho desmenuzado para pavimento de parques infantiles.
+      .col-lg-3.mb-4
+        .tarjeta.color-11.py-2.px-0 
+          .row.align-items-center
+            .col-lg-4
+              figure
+                img(src='@/assets/curso/tema5/4.svg', alt='Texto que describa la imagen')
+            .col-lg-8
+              .row
+                .col-lg-11
+                  p.mb-0 Botellas de vidrio convertidas en aislante acústico, térmico y antihumedad, como el vidrio celular.
+      
+      .col-lg-3.mb-4
+        .tarjeta.color-11.py-2.px-0 
+          .row.align-items-center
+            .col-lg-4
+              figure
+                img(src='@/assets/curso/tema5/5.svg', alt='Texto que describa la imagen')
+            .col-lg-8
+              .row
+                .col-lg-11
+                  p.mb-0 Neumáticos viejos transformados en suelas de zapatos.
+      .col-lg-3.mb-4
+        .tarjeta.color-11.py-2.px-0 
+          .row.align-items-center
+            .col-lg-4
+              figure
+                img(src='@/assets/curso/tema5/6.svg', alt='Texto que describa la imagen')
+            .col-lg-8
+              .row
+                .col-lg-11
+                  p.mb-0 Ropa usada como material para fabricar aislantes térmicos en bioconstrucción.
+      .col-lg-3.mb-4
+        .tarjeta.color-11.py-2.px-0 
+          .row.align-items-center
+            .col-lg-4
+              figure
+                img(src='@/assets/curso/tema5/7.svg', alt='Texto que describa la imagen')
+            .col-lg-8
+              .row
+                .col-lg-11
+                  p.mb-0 Caucho desmenuzado para pavimento de parques infantiles.
+      .col-lg-3.mb-4
+        .tarjeta.color-11.py-2.px-0 
+          .row.align-items-center
+            .col-lg-4
+              figure
+                img(src='@/assets/curso/tema5/8.svg', alt='Texto que describa la imagen')
+            .col-lg-8
+              .row
+                .col-lg-11
+                  p.mb-0 Botellas de vidrio convertidas en aislante acústico, térmico y antihumedad, como el vidrio celular.
+    
+    .row.align-items-center.justify-content-center.mb-4
+      .col-lg-3.mb-4
+        .tarjeta.color-11.py-2.px-0 
+          .row.align-items-center
+            .col-lg-4
+              figure
+                img(src='@/assets/curso/tema5/9.svg', alt='Texto que describa la imagen')
+            .col-lg-8
+              .row
+                .col-lg-11
+                  p.mb-0 Separadores de carriles bici de aparatos electrónicos.
+      .col-lg-3.mb-4
+        .tarjeta.color-11.py-2.px-0 
+          .row.align-items-center
+            .col-lg-4
+              figure
+                img(src='@/assets/curso/tema5/10.svg', alt='Texto que describa la imagen')
+            .col-lg-8
+              .row
+                .col-lg-11
+                  p.mb-0 Plástico usado para crear mobiliario urbano nuevo.
 
 </template>
 
